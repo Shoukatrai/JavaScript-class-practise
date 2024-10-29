4e45876c35a8bf46f71589a50911b84166e9f135
Shoukatrai <raishoukat033@gmail.com>
2024-10-29 15:01:22 +0500

    Class 14 practise

---
+++ Class 14/app.js
@@ -0,0 +1,67 @@
+// var input = document.getElementById("userName");
+// console.log("userName", userName)
+// function submitForm(){
+//     console.log("input", input.value)
+// }
+
+// var para = document.getElementById("para");
+// function getPara(){
+//     console.log(para.innerHTML)
+// }
+
+// function setPara(){
+//     // para.innerHTML = "SHOUKAT UMEDANI";
+//     para.innerHTML += "SHOUKAT UMEDANI";
+// }
+
+// function greet(){
+//     alert("Hello Shoukat");
+// }
+
+
+
+// function foo(){
+//     console.log("foo()");
+// }
+
+
+var carImg = document.getElementById("carImg")
+
+
+// function imgHandler(){
+//     // console.log(carImg.src)
+//     carImg.src = "./asset/car1.gif"
+// }
+
+
+// function imgOutHandler(){
+//     // console.log(carImg.src)
+//     carImg.src = "./asset/car1.jpeg"
+// }
+
+
+// function imgHandler(){
+//     // console.log(carImg.src)
+//     carImg.src = "./asset/car2.gif"
+// }
+
+
+// function imgOutHandler(){
+//     // console.log(carImg.src)
+//     carImg.src = "./asset/car2.jpeg"
+// }
+
+
+
+
+// function focusInput(element){
+//     element.style.color = "blue";
+// }
+
+
+
+// function blurInput(element){
+// if(element.value.length < 6){
+//     element.style.color = "red";
+// }
+// }
