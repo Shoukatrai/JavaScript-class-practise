a0af8f1d8a6c9d1364d7ab1eb4a722568d6ce6af
Shoukatrai <raishoukat033@gmail.com>
2024-11-05 13:18:33 +0500

    claa 16 dOM and text editor practise

---
+++ Class 16/app.js
@@ -0,0 +1,79 @@
+function paraHandler(btnType){
+    // var heading = document.getElementById("heading")
+    
+    // heading.className += " BigText"
+
+    // heading.classList.remove("smallText")
+    // heading.classList.add("bigText")
+
+// heading.classList.replace("smallText" , "bigText")
+
+    // if(btnType =="big"){
+    //     heading.className = "bigText"
+    // }else{
+    //     heading.className = "smallText"
+    // }
+
+}
+
+
+
+// function toogleHandler(){
+//     var heading = document.getElementById("heading");
+//     var classContain = heading.classList.contains("smallText")
+    
+//     if(classContain){
+//         heading.classList.replace("smallText" , "bigText")
+//     }else{
+//         heading.classList.replace("bigText" , "smallText")
+//     }
+// }
+
+
+// var textNumber = document.getElementById("textNumber")
+// var para = document.getElementById("para")
+
+// function fontSizeCounter(btnType){
+//     var counter = textNumber.innerHTML;
+//     // textNumber.innerHTML = ++counter;
+//     if(btnType == "plus"){
+//         textNumber.innerHTML = ++counter;
+//     }else{
+//         textNumber.innerHTML = --counter;
+//     }
+//     para.style.fontSize = `${counter}px`
+// }
+
+
+
+// function colorChange(input){
+//     // console.log(input.value)
+//     para.style.color = input.value;
+// }
+
+// function makeBold(){
+//     para.style.fontWeight = "bold"
+// }
+
+// function makeItalic(){
+//     para.style.fontStyle = "italic"
+// }
+
+// function positionChange(){
+//     para.style.position = "relative"
+//     para.style.top = "20px"
+// }
+
+// var paras = document.getElementsByTagName("p")
+// console.log(paras)
+
+
+// var a = document;
+// console.log(a.childNodes[1])
+
+console.log(document.childNodes[1].childNodes[2].childNodes)
+
+
+
+
+
