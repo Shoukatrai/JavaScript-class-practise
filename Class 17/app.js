61352c0b08e48a70b7559bfdc7e38737ae42aa00
Shoukatrai <raishoukat033@gmail.com>
2024-11-06 21:50:21 +0500

    class 17 practise in class time

---
+++ Class 17/app.js
@@ -0,0 +1,29 @@
+// console.log(document.children[0].children[1].children[0])
+// var parent = document.children[0].children[1].children[0];
+// console.log(parent.childNodes)
+// console.log(parent.children)
+// console.log(parent.firstChild)
+// console.log(parent.lastChild)
+// console.log(parent.firstElementChild)
+// var para1 = parent.firstChild;
+// var para1 = parent.firstElementChild;
+// var para4 = parent.lastElementChild;
+// console.log(para4.nextSibling)
+// console.log(para4.nextElementSibling)
+// console.log(para4.previousElementSibling)
+// var para3 = para4.previousElementSibling;
+// console.log(para3)
+
+var parent = document.querySelector("#parent")
+// console.log("parent", parent.children)
+// console.log("parent", parent.childNodes)
+// console.log("para1", parent.firstElementChild)
+// console.log("para2", parent.firstElementChild.nextElementSibling)
+// console.log("para3", parent.firstElementChild.nextElementSibling.nextElementSibling)
+// console.log("para4", parent.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling)
+console.log("para4", parent.lastElementChild)
+
+
+
+
+
