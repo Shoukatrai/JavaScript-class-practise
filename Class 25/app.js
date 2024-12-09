7ecda6779b5adb2dc62befec8961fdd481709b6f
Shoukatrai <raishoukat033@gmail.com>
2024-12-09 13:20:27 +0500

    class 25 practice

---
+++ Class 25/app.js
@@ -0,0 +1,52 @@
+// console.log("Hello World")
+
+// object destructuring
+// const objectNew = {
+//     name: "shoukat",
+//     age: 23
+// }
+
+// console.log(objectNew.name, objectNew.age)
+// const{name, age} = objectNew
+// console.log(name)
+// console.log(age)
+
+// const stdObj = {
+//     firstName:"Shoukat",
+//     lastName:"Rai",
+//     age:20,
+//     religion:"Hinduisim"
+// }
+
+// // console.log(name,age)
+// const {firstName,lastName,age,religion} = stdObj
+// console.log(firstName,lastName,age,religion)
+// const {firstName : name , lastName: testName} = stdObj
+// console.log(testName)
+
+
+// array destructuring
+// const array = [10,20,30,40,50]
+// const[a,b,c,d,e] = array
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+// const array = [10,20,30,40,50]
+// const[a,b,c,d,e,f] = array
+// console.log(a)
+
+// skip Element
+// const array = [10,20,30,40,50]
+// const [_,e] = array
+// console.log(e)
+
+// default value 
+// const array = [10,20]
+// const [val1,val2,val3 = 0] = array
+// console.log(val1,val2,val3)
+
+
+// the spread (...) opeartor
