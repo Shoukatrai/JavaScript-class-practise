aec71a6deddb9ae79c8a3ddc4cc6e49808989414
Shoukatrai <raishoukat033@gmail.com>
2024-12-19 14:39:18 +0500

    nulesh operator, ternary operator

---
+++ Class 29/app.js
@@ -0,0 +1,59 @@
+// console.log("app.js")
+
+
+
+// import name  from "./script.js";
+// console.log(name)
+
+// import{age , firstName , lastName} from "./script.js"
+// console.log(age , firstName , lastName)
+
+
+//trenary operator 
+//Note: ternary operator are used as if else , syntax difference, only one line output , only 3 nested conditions
+
+// const age = 18;
+// if (age== 18){
+//     console.log("Allow")
+// } else{
+//     console.log("Not Allow")
+// // }
+// const check = age ==18 ? "allow" : "Don't Allow"
+// console.log(check) 
+
+
+// ? => if 
+// : => else 
+
+// const a = 15 
+// const isCheck = a == 15 ? "Allow" : "Don;t allow"
+// console.log(isCheck) 
+
+
+
+// if => ? 
+// else if 
+// else => : 
+
+// // 18 , 25 , 30
+// const age = 18; 
+// const result = age  == 18 ? "age is 18" : age == 25 ? "age is 25" : age == 30 ? "age is 30": "Dosen't match"
+// console.log(result)
+
+
+
+
+
+// var isValue = false // var isCheck = isValue ?? [] // console.log(isCheck) var age = 18 //backend if(!age){ console.log("not allow") }
+
+
+//nulesh operator
+
+//null ya undefined value ho to
+// let isValue = undefined
+// let isCheck = isValue ?? []
+// console.log(isCheck)
+
+// let isValue = null
+// let isCheck = isValue ?? 0
+// console.log(isCheck)
---
+++ Class 29/script.js
@@ -0,0 +1,12 @@
+const name = "shoukat";
+export default name
+
+const firstName = "Shoukat";
+const lastName  = "Rai";
+const age = 20;
+
+export  {
+    firstName, lastName , age
+}
+
+
